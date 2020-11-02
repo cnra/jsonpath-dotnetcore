629d9cd8c5cb70c16d389da5fb91f65c8e8ee256
cnra <roser137@hotmail.com>
2020-11-02 12:43:43 +0300

    Add project files.

---
+++ jsonpath-dotnetcore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace jsonpath_dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var txt = File.ReadAllText("data.json");
+            JObject js = JObject.Parse(txt);
+            var res = js.SelectToken("$.header.components[?(@.id=='chapterHeaderVersion')].value");
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
